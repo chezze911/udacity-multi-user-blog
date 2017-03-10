7e9a4773a40bef856128bf4a50efc97b20d25c99
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-03-10 15:13:30 -0800

    update permalink.html file to have user input like and comments

--- blog.py
+++ blog.py
@@ -157,7 +157,7 @@ PASS_RE = re.compile(r"^.{3,20}$")
 def valid_password(password):
     return password and PASS_RE.match(password)
 
-# Defines a. valid email
+# Defines a valid email
 EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
