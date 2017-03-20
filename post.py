c69c1ad90987bf25902031a3b6518cd740f3d3f9
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-03-19 20:22:24 -0700

    Update post.py file check code for PEP8 requirements

--- post.py
+++ post.py
@@ -8,23 +8,25 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
+
 def jinja_render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-# Hold blog post info. 
+
+# Hold blog post info.
 class Post(db.Model):
-    
-    user_id = db.IntegerProperty(required = True)
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
+
+    user_id = db.IntegerProperty(required=True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
 
     def getUserName(self):
         user = User.by_id(self.user_id)
         return user.name
-    
+
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
-        return jinja_render_str("post.html", p = self)
+        return jinja_render_str("post.html", p=self)
