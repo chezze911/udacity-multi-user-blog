8b56f59976c01a88d865614402410215822be746
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-03-19 20:18:44 -0700

    Update check user.py file for PEP8 requirements

--- user.py
+++ user.py
@@ -4,37 +4,42 @@ import hashlib
 from string import letters
 from google.appengine.ext import db
 
-##### user stuff
+
+# user stuff
 # Make salt to secure password.
-def make_salt(length = 5):
+def make_salt(length=5):
     return ''.join(random.choice(letters) for x in xrange(length))
 
+
 # Make password hash containing name, pw, and a salt.
-def make_pw_hash(name, pw, salt = None):
+def make_pw_hash(name, pw, salt=None):
     if not salt:
         salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (salt, h)
+
 
 # Check that pw is valid by hashing and comparing it to existing hash pw.
 def valid_pw(name, password, h):
     salt = h.split(',')[0]
     return h == make_pw_hash(name, password, salt)
 
+
 # Get key from user table.
-def users_key(group = 'default'):
+def users_key(group='default'):
     return db.Key.from_path('users', group)
+
 
 # Creates a database to store user information.
 class User(db.Model):
-    name = db.StringProperty(required = True)
-    pw_hash = db.StringProperty(required = True)
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
     email = db.StringProperty()
 
-    # Get user by user id.  
+    # Get user by user id.
     @classmethod
     def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
+        return User.get_by_id(uid, parent=users_key())
 
     # Get user by name.
     @classmethod
@@ -42,16 +47,16 @@ class User(db.Model):
         u = User.all().filter('name =', name).get()
         return u
 
-    # Register by first hashing password .  
+    # Register by first hashing password .
     @classmethod
-    def register(cls, name, pw, email = None):
+    def register(cls, name, pw, email=None):
         pw_hash = make_pw_hash(name, pw)
-        return User(parent = users_key(),
-                    name = name,
-                    pw_hash = pw_hash,
-                    email = email)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
 
-    # Login by first checking password.  
+    # Login by first checking password.
     @classmethod
     def login(cls, name, pw):
         u = cls.by_name(name)
