173d08527f8b84da1c73e59d6ce059510be6b89d
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-03-20 19:02:40 -0700

    Update blog.py file

--- blog.py
+++ blog.py
@@ -110,7 +110,8 @@ class PostPage(BlogHandler):
 
         # Render the page and show blog content, comments, likes, etc.
         self.render("permalink.html", post=post, comments=comments,
-                    numOfLikes=likes.count(), error=error)
+                    numOfLikes=likes.count(),error=error)
+
 
     def post(self, post_id):
         # get all the necessary parameters
@@ -156,6 +157,108 @@ class PostPage(BlogHandler):
 
         self.redirect('/blog/%s' % post_id)
 
+# class NewComment(BlogHandler):
+#     def get(self, post_id):
+#         # Get key from blog post.
+#         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+#         post = db.get(key)
+#         comments = db.GqlQuery("select * from Comment where post_id = " +
+#                                post_id + " order by created desc")
+
+#         # if the post doesn't exist throw an error
+#         if not post:
+#             self.error(404)
+#             return
+        
+#         error = self.request.get("error")
+         
+#         self.render("permalink.html", post=post, comments=comments, 
+#                     error=error)
+        
+#     def post(self, post_id):
+#         # get all the necessary parameters
+#         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+#         post = db.get(key)
+#         c = ""
+#         comments = db.GqlQuery("select * from Comment where post_id = " +
+#                                post_id + " order by created desc")
+
+#         # Check first if the post exists and throw an error if it doesn't
+#         if not post:
+#             self.error(404)
+#             return
+
+#         # Check if user is signed in.
+#         if self.user: 
+#             # Check if user is trying t olike his own post.
+#             if self.user.key().id() == post.user_id:
+#                 self.write("You cannot comment on your own post!")
+#                 return
+#             # Commenting on a post creates a new comment tuple.
+#             if(self.request.get('comment')):
+#                 c = Comment(parent=blog_key(), user_id=self.user.key().id(),
+#                             post_id=int(post_id),
+#                             comment=self.request.get('comment'))
+#                 c.put()
+#         else:
+#             self.redirect('/loginError')
+#             return
+
+#         self.redirect('/blog/%s' % post_id)
+
+# class LikePost(BlogHandler):
+#     def get(self, post_id):
+#         # Get key from blog post.
+#         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+#         post = db.get(key)
+#         likes = db.GqlQuery("select * from Like where post_id=" + post_id)
+
+#         # if the post doesn't exist throw an error
+#         if not post:
+#             self.error(404)
+#             return
+#         error = self.request.get("error")
+
+#         # Render the page and show blog content, comments, likes, etc.
+#         self.render("permalink.html", post=post, numOfLikes=likes.count(), 
+#                     error=error)
+
+#     def post(self, post_id):
+#         # get all the necessary parameters
+#         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+#         post = db.get(key)
+#         likes = db.GqlQuery("select * from Like where post_id = " + post_id)
+
+#         # Check first if the post exists and throw an error if it doesn't
+#         if not post:
+#             self.error(404)
+#             return
+
+#         # Check if user is signed in.
+#         if self.user:
+#             # When like is clicked, like value increases by 1
+#             if(self.request.get('like') and self.request.get('like') ==
+#                "updateLike"):
+#                 likes = db.GqlQuery("select * from Like where post_id =" +
+#                                     post_id + " and user_id = " +
+#                                     str(self.user.key().id()))
+#             print likes.count()
+#             # Check if user is trying t olike his own post.
+#             if self.user.key().id() == post.user_id:
+#                 self.write("You cannot like on your own post!")
+#                 return
+#             # add 1 to likes count if value is 0
+#             elif likes.count() == 0:
+#                 l = Like(parent=blog_key(), user_id=self.user.key().id(),
+#                          post_id=int(post_id))
+#                 l.put()
+
+#         else:
+#             self.redirect('/loginError')
+#             return
+
+#         self.redirect('/blog/%s' % post_id)
+        
 
 class EditPost(BlogHandler):
     def get(self, post_id):
@@ -182,8 +285,12 @@ class EditPost(BlogHandler):
             self.redirect('/loginError')
 
     def post(self, post_id):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
+        key = db.Key.from_path(
+                               'Post',
+                               int(post_id),
+                               parent=blog_key()
+                               )
+        post = db.get(key)
         # if the post doesn't exist throw an error
         if not post:
             self.error(404)
@@ -191,6 +298,9 @@ class EditPost(BlogHandler):
         # Check if user is not logged in.
         if not self.user:
             self.redirect('/blog')
+        
+        subject = self.request.get('subject')
+        content = self.request.get('content')
 
         if subject and content:
             # Get all the necessary post parameters
@@ -528,5 +638,7 @@ app = webapp2.WSGIApplication([('/?', BlogFront),
                                 EditComment),
                                ('/blog/deletecomment/([0-9]+)/([0-9]+)',
                                 DeleteComment),
+                               # ('/blog/like/([0-9]+)', LikePost)
+                               # ('/blog/newcomment/([0-9]+)', NewComment),
                                ],
                               debug=True)
